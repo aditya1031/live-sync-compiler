913104ae9779bbb53c1ec4c1b90a46ec449cf249
Aditya1031 <Adityasingh1031@gmail.com>
2023-09-21 18:49:32 +0530

    form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -103,25 +103,118 @@ const Home = () => (
       <div className='peimg' id='aditya'>
         <svg xmlns="http://www.w3.org/2000/svg" width="285" height="298" viewBox="0 0 285 298" fill="none">
           <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
-          <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
         </svg>
+      </div>
+      <div className='info'>
+        <div className='title'>
+          <h3 className='name'>Aditya Kumar</h3>
+          <h3 className='job'>-Team leader</h3>
+        </div>
+        <p className='about'>
+          Aditya, as the team leader, played a pivotal role in the development of our project. With expertise in backend development, he spearheaded the architectural decisions and ensured smooth and robust functionality.
+
+          His deep knowledge in Docker enabled seamless integration, ensuring scalability and portability for our platform.
+
+          Additionally, Aditya's flair for design was evident as he also contributed to the web designing aspect, ensuring our platform is not only functional but also aesthetically appealing.
+        </p>
+        <button type="button" className='batan' id='batabout'> link tree</button>
       </div>
 
-      <div className='peimg' id='priyashi'>
+
+
+      <div className='peimg' id='priyashi' >
         <svg xmlns="http://www.w3.org/2000/svg" width="285" height="298" viewBox="0 0 285 298" fill="none">
           <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
-          <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
         </svg>
+
       </div>
+      <div className='info' id='priya'>
+        <div className='title'>
+          <h3 className='name' id='priyaname'>Priyashi</h3>
+          <h3 className='job' id='priyajob'> -react front-end</h3>
+        </div>
+        <p className='about' id='priyaabout'>
+          Aditya, as the team leader, played a pivotal role in the development of our project. With expertise in backend development, he spearheaded the architectural decisions and ensured smooth and robust functionality.
+
+          His deep knowledge in Docker enabled seamless integration, ensuring scalability and portability for our platform.
+
+        </p>
+        <button type="button" className='batan' id='batabout' >link tree </button>
+      </div>
+
+
 
       <div className='peimg' id='joti'>
         <svg xmlns="http://www.w3.org/2000/svg" width="285" height="298" viewBox="0 0 285 298" fill="none">
           <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
-          <path d="M285 68.9485C285 143.182 210.175 298 139.313 298C68.4511 298 0 143.182 0 68.9485C0 -5.28478 68.4511 0.0858125 139.313 0.0858125C210.175 0.0858125 285 -5.28478 285 68.9485Z" fill="#D9D9D9" />
         </svg>
+      </div>
+      <div className='info'>
+        <div className='title'>
+          <h3 className='name'>Joti</h3>
+          <h3 className='job'>-documentation</h3>
+        </div>
+        <p className='about'>
+
+          Aditya, as the team leader, played a pivotal role in the development of our project. With expertise in backend development, he spearheaded the architectural decisions and ensured smooth and robust functionality.
+
+          His deep knowledge in Docker enabled seamless integration, ensuring scalability and portability for our platform.
+
+          Additionally, Aditya's flair for design was evident as he also contributed to the web designing aspect, ensuring our platform is not only functional but also aesthetically appealing.
+        </p>
+        <button type="button" className='batan' id='batabout'>link tree </button>
       </div>
 
 
+    </div>
+
+
+
+
+
+
+    <div>
+
+      <h1 className='topics' id='contributors '>contact us </h1>
+
+      <div className="form">
+
+        <div className="name-email-container"> {/* New container for name and email */}
+          <div className='texte' id='name'>
+            <input type="text" id="name" name="name" />
+          </div>
+          <div className='texte' id='email'>
+            <input type="email" id="email" name="email" />
+          </div>
+        </div>
+
+
+        <div className='texte' id='sub'>
+          <input type="text" id="sub" name="sub" />
+        </div>
+        <div className='texte' id='phone'>
+          <input type="tel" id="phone" name="phone" />
+        </div>
+        <div className='texte' id='mess'>
+          <textarea id="mess" name="mess" rows="6" cols="20"></textarea>
+        </div>
+        <button type="button" className='batan' id='submit' >submit</button>
+      </div>
+
+      <div>
+        <h1>
+        <button type="button" className='baton'></button>
+        <button type="button" className='baton' >sdjdah</button>
+        <button type="button" className='baton' >UP</button>
+        <button type="button" className='baton' >UP</button>
+        </h1>
+      </div>
+
+
+      <div className='footer'>
+        <h3 id='copr'>© Copyright 2023, All Rights Reserved          |            Adityasingh1031@gmail.com</h3>
+        <button type="button" className='batan' id='up'>UP</button>
+      </div>
     </div>
 
   </>
